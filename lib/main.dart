39fb5eb8e0a57b4e6ac864becffca92a827fe99f
freitaspedro <pfreitas@id.uff.br>
2023-05-02 17:22:19 -0300

    add get story choices methods

--- lib/main.dart
+++ lib/main.dart
@@ -63,7 +63,7 @@ class _StoryPageState extends State<StoryPage> {
                   ),
                   child: Text(
                     //TODO: Step 13 - Use the storyBrain to get the text for choice 1.
-                    'Choice 1',
+                    storyBrain.getChoice1(),
                     style: TextStyle(
                       fontSize: 20.0,
                     ),
@@ -87,7 +87,7 @@ class _StoryPageState extends State<StoryPage> {
                   ),
                   child: Text(
                     //TODO: Step 14 - Use the storyBrain to get the text for choice 2.
-                    'Choice 2',
+                    storyBrain.getChoice2(),
                     style: TextStyle(
                       fontSize: 20.0,
                     ),
